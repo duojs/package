9e27a251306941e46cff41c9e83060b748cc5c48
Matthew Mueller <mattmuelle@gmail.com>
2014-02-20 22:52:18 +0100

    remove auth token

--- example.js
+++ example.js
@@ -5,15 +5,16 @@
 var co = require('co');
 var thunkify = require('thunkify');
 var Package = require('./');
-var pkg = new Package('matthewmueller/cheerio', 'master')
-  .auth('matthewmueller', '38c9849316796e0550e8e4aeabdd3e98b8f40a2f')
+var pkg = new Package('matthewmueller/uid', 'master')
+  .auth('matthewmueller', process.env.token)
   .to('node_modules');
 
-// pkg.read('component.json', function(err, contents) {
-//   console.log(err, contents);
-// })
-
-pkg.fetch(function(err) {
+pkg.read('component.json', function(err, contents) {
   if (err) throw err;
-  console.log('all done!');
+  console.log(contents);
 })
+
+// pkg.fetch(function(err) {
+//   if (err) throw err;
+//   console.log('all done!');
+// })
