6eb7ff2b75de65addea6703c57d2630d2d9cb0b8
Amir Abu Shareb <yields@icloud.com>
2014-04-27 08:13:34 +0300

    add events

--- example.js
+++ example.js
@@ -4,15 +4,21 @@
 
 var co = require('co');
 var Package = require('./');
-var pkg = new Package('matthewmueller/cheerio', '*')
+var pkg = new Package('cheeriojs/cheerio', '*')
   .auth(process.env.user, process.env.token)
   .directory('node_modules');
 
 co(function *() {
-
-  return yield pkg.read('package.json');
-
+  pkg.on('fetching', log(pkg, 'fetching'));
+  pkg.on('fetched', log(pkg, 'fetched'));
+  yield pkg.fetch();
 })(function(err, pkg) {
   if (err) throw err;
   console.log(pkg);
 });
+
+function log(pkg, str){
+  return function(){
+    console.log(str + ' : %s', pkg.slug());  
+  };
+}
