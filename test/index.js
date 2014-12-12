4a0007a4c2fcfa6832a9cc4ce1183a22e17e8bb8
Stephen Mathieson <smath23@gmail.com>
2014-12-12 16:30:33 -0500

    lib: Remove `.netrc` magic
    This patch removes all `~/.netrc`-related logic from duo-package
    entirely.  Configuration file parsing should be handled upstream in
    `duo(1)`, not in a library.
    
    Removing this magic will make adding credentials for other providers
    significantly easier (and more verbose) when we add support for such
    behaviour.
    
    Closes #50.

--- test/index.js
+++ test/index.js
@@ -3,10 +3,17 @@ var exists = require('fs').existsSync;
 var mkdirp = require('mkdirp').sync;
 var rimraf = require('rimraf').sync;
 var assert = require('assert');
+var netrc = require('node-netrc');
 var Package = require('..');
 
 describe('duo-package', function(){
   var pkgs = ['component/type@master', 'component/type@1.0.0', 'component/emitter@master'];
+  var token = null;
+
+  before(function(){
+    var auth = netrc('api.github.com') || {};
+    token = auth.password;
+  });
 
   beforeEach(function(){
     mkdirp(__dirname + '/tmp');
@@ -24,6 +31,7 @@ describe('duo-package', function(){
     yield pkgs.map(function(pkg){
       var parts = pkg.split('@');
       pkg = Package(parts[0], parts[1]);
+      pkg.token(token);
       pkg.directory(__dirname + '/tmp');
       return pkg.fetch();
     });
@@ -36,6 +44,7 @@ describe('duo-package', function(){
   it('should error when package is not found (status code: 406)', function*(){
     var pkg = Package('component/406', '1.0.0');
     pkg.directory(__dirname + '/tmp');
+    pkg.token(token);
     var msg;
 
     try {
@@ -52,6 +61,7 @@ describe('duo-package', function(){
     this.timeout(60000);
     var pkg = Package('twbs/bootstrap', 'v3.2.0');
     pkg.directory(__dirname + '/tmp');
+    pkg.token(token);
     yield pkg.fetch();
     assert(exists(__dirname + '/tmp/twbs-bootstrap@v3.2.0/package.json'));
   })
@@ -62,9 +72,13 @@ describe('duo-package', function(){
     var c = Package('component/tip', '1.x');
     var d = Package('component/tip', '1.x');
     a.directory(__dirname + '/tmp');
+    a.token(token);
     b.directory(__dirname + '/tmp');
+    b.token(token);
     c.directory(__dirname + '/tmp');
+    c.token(token);
     d.directory(__dirname + '/tmp');
+    d.token(token);
     yield [a.fetch(), b.fetch(), c.fetch(), d.fetch()];
     assert(exists(__dirname + '/tmp/component-tip@1.0.3/component.json'));
   })
@@ -72,12 +86,14 @@ describe('duo-package', function(){
   it('should work with renamed repos', function *() {
     var pkg = Package('component/get-document', '0.1.0');
     pkg.directory(__dirname + '/tmp')
+    pkg.token(token);
     yield pkg.fetch();
     assert(exists(__dirname + '/tmp/component-get-document@0.1.0/component.json'));
   })
 
   it('should work with callbacks', function(done) {
     var pkg = Package('component/emitter', '0.0.x');
+    pkg.token(token);
     pkg.resolve(function(err, ref) {
       if (err) done(err);
       assert('0.0.6' == ref);
@@ -87,6 +103,7 @@ describe('duo-package', function(){
 
   it('should work on weird forked semvers', function(done){
     var pkg = Package('segmentio/marked', '*');
+    pkg.token(token);
     pkg.resolve(function (err, ref) {
       if (err) return done(err);
       assert(/v[.\d]+/.test(ref));
