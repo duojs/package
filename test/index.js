5d21400be90eb3b9ca76e0441f7a6b96be28c32f
Dominic Barnes <dominic@dbarnes.info>
2015-07-05 01:36:48 -0700

    eslint

--- test/index.js
+++ test/index.js
@@ -1,10 +1,13 @@
 
+var assert = require('assert');
 var exists = require('co-fs').exists;
 var mkdirp = require('mkdirp-then');
-var rimraf = require('rimraf-then');
-var assert = require('assert');
 var netrc = require('node-netrc');
 var Package = require('..');
+var path = require('path');
+var rimraf = require('rimraf-then');
+
+var tmp = path.join(__dirname, 'tmp');
 
 describe('duo-package', function(){
   var pkgs = ['component/type@master', 'component/type@1.0.0', 'component/emitter@master'];
@@ -16,34 +19,34 @@ describe('duo-package', function(){
   });
 
   beforeEach(function*(){
-    yield mkdirp(__dirname + '/tmp');
-  })
+    yield mkdirp(tmp);
+  });
 
   before(function*(){
-    yield rimraf(__dirname + '/tmp');
-  })
+    yield rimraf(tmp);
+  });
 
   afterEach(function*(){
-    yield rimraf(__dirname + '/tmp');
-  })
+    yield rimraf(tmp);
+  });
 
   it('should install correctly', function*(){
     yield pkgs.map(function(pkg){
       var parts = pkg.split('@');
       pkg = Package(parts[0], parts[1]);
       pkg.token(token);
-      pkg.directory(__dirname + '/tmp');
+      pkg.directory(tmp);
       return pkg.fetch();
     });
 
-    assert(yield exists(__dirname + '/tmp/component-type@1.0.0/component.json'));
-    assert(yield exists(__dirname + '/tmp/component-type@master/component.json'));
-    assert(yield exists(__dirname + '/tmp/component-emitter@master/component.json'));
-  })
+    assert(yield exists(path.join(tmp, 'component-type@1.0.0/component.json')));
+    assert(yield exists(path.join(tmp, 'component-type@master/component.json')));
+    assert(yield exists(path.join(tmp, 'component-emitter@master/component.json')));
+  });
 
   it('should error when package is not found (status code: 404)', function*(){
     var pkg = Package('component/404', '1.0.0');
-    pkg.directory(__dirname + '/tmp');
+    pkg.directory(tmp);
     pkg.token(token);
     var msg;
 
@@ -54,51 +57,51 @@ describe('duo-package', function(){
     }
 
     assert(~msg.indexOf('component-404@1.0.0: returned with status code: 404'));
-  })
+  });
 
   it('should work with bootstrap', function *() {
     this.timeout(60000);
     var pkg = Package('twbs/bootstrap', 'v3.2.0');
-    pkg.directory(__dirname + '/tmp');
+    pkg.directory(tmp);
     pkg.token(token);
     yield pkg.fetch();
-    assert(yield exists(__dirname + '/tmp/twbs-bootstrap@v3.2.0/package.json'));
-  })
+    assert(yield exists(path.join(tmp, 'twbs-bootstrap@v3.2.0/package.json')));
+  });
 
   it('should handle inflight requests', function *() {
     var a = Package('component/tip', '1.x');
     var b = Package('component/tip', '1.x');
     var c = Package('component/tip', '1.x');
     var d = Package('component/tip', '1.x');
-    a.directory(__dirname + '/tmp');
+    a.directory(tmp);
     a.token(token);
-    b.directory(__dirname + '/tmp');
+    b.directory(tmp);
     b.token(token);
-    c.directory(__dirname + '/tmp');
+    c.directory(tmp);
     c.token(token);
-    d.directory(__dirname + '/tmp');
+    d.directory(tmp);
     d.token(token);
     yield [a.fetch(), b.fetch(), c.fetch(), d.fetch()];
-    assert(yield exists(__dirname + '/tmp/component-tip@1.0.3/component.json'));
-  })
+    assert(yield exists(path.join(tmp, 'component-tip@1.0.3/component.json')));
+  });
 
   it('should work with renamed repos', function *() {
     var pkg = Package('component/get-document', '0.1.0');
-    pkg.directory(__dirname + '/tmp')
+    pkg.directory(tmp);
     pkg.token(token);
     yield pkg.fetch();
-    assert(yield exists(__dirname + '/tmp/component-get-document@0.1.0/component.json'));
-  })
+    assert(yield exists(path.join(tmp, 'component-get-document@0.1.0/component.json')));
+  });
 
   it('should work with callbacks', function(done) {
     var pkg = Package('component/emitter', '0.0.x');
     pkg.token(token);
     pkg.resolve(function(err, ref) {
       if (err) done(err);
-      assert('0.0.6' == ref);
+      assert.equal(ref, '0.0.6');
       done();
-    })
-  })
+    });
+  });
 
   it('should work on weird forked semvers', function(done){
     var pkg = Package('segmentio/marked', '*');
@@ -108,12 +111,12 @@ describe('duo-package', function(){
       assert(/v[.\d]+/.test(ref));
       done();
     });
-  })
+  });
 
   describe('private modules', function() {
     it('should throw a meaningful error when not authenticated', function (done) {
       var pkg = Package('matthewmueller/wordsmith', 'master');
-      pkg.directory(__dirname + '/tmp')
+      pkg.directory(tmp);
       pkg.token(null);
       pkg.fetch(function(err) {
         assert(err);
@@ -123,7 +126,7 @@ describe('duo-package', function(){
     });
 
     // TODO: figure out a way to test private modules
-  })
+  });
 
   describe('cache', function () {
     it('should clean the tmp dir cache', function *() {
@@ -132,4 +135,4 @@ describe('duo-package', function(){
       assert(!(yield exists(Package.cachepath)));
     });
   });
-})
+});
